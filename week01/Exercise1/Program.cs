b97dadf445944d8998398b4dccc162e37c39c3c9
J.Alexis Sanchez <johanalexis54@gmail.com>
2026-01-08 20:17:20 -0500

    My first exercise in C#

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,22 @@
+
 using System;
 
-class Program
+namespace game
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name? ");
+            string first = Console.ReadLine ();
+            Console.WriteLine("What is your last name? ");
+            string last = Console.ReadLine ();
+            Console.WriteLine(); 
+            Console.WriteLine($"Your name is {last}, {first} {last}");
+            Console.WriteLine();             
+
+
+        }    
     }
+
 }
