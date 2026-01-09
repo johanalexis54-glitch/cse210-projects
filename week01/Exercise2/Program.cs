8529fff486f64370a72158086f764df7bcd38e45
J.Alexis Sanchez <johanalexis54@gmail.com>
2026-01-09 17:39:41 -0500

    Second Exercise teach me to remmember many concepts

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string Grade = Console.ReadLine();
+        int GradeN = int.Parse(Grade);
+        string score = "";
+        int LastDigit = GradeN % 10;
+        if (LastDigit == 7)
+        {
+            score = "+";
+        }
+        else if (LastDigit == 3)
+        {
+            score = "-";
+        }
+        else
+        {
+            score = "";
+        }
+
+        if (GradeN >= 90)
+        {
+           if (LastDigit == 7)
+            {
+                Console.WriteLine($"Your Grade is A"); 
+            }
+            else
+            {
+                Console.WriteLine($"Your Grade is A{score}"); 
+            }
+        }
+        else if (GradeN < 90 && GradeN >= 80)
+        {
+            Console.WriteLine($"Your Grade is B{score}"); 
+        }
+        else if (GradeN < 80 && GradeN >= 70)
+        {
+            Console.WriteLine($"Your Grade is C{score}"); 
+        }
+        else if (GradeN < 70 && GradeN >= 60)
+        {
+            Console.WriteLine($"Your Grade is D{score}"); 
+        }
+        else
+        {
+            if (LastDigit == 7 || LastDigit == 3)
+            {
+                Console.WriteLine($"Your Grade is F");
+            }
+            else
+            {
+                Console.WriteLine($"Your Grade is F{score}");
+            }
+        }
+
     }
 }
