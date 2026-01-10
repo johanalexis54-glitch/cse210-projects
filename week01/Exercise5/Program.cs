699b418efeda0d5f2c39042e6dac86f4a65482c5
J.Alexis Sanchez <johanalexis54@gmail.com>
2026-01-10 11:22:06 -0500

    Finish this week

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Welcome
+        static void DisplayPersonalMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        } DisplayPersonalMessage();
+
+        // Enter Name and save name.
+        static string UserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;    
+        }
+
+        // Enter the number
+        static float FNumber()
+        {
+            Console.Write("Please enter your favorit number: ");
+            string numbers = Console.ReadLine();
+            float number = float.Parse(numbers);
+            float square = number * number;
+            return square;
+        }
+        Console.WriteLine(UserName() + ", the square of your number is " + FNumber());
+
     }
 }
