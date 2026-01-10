919a16d32a60294a8d5ccff05c25258cbbbe72ae
J.Alexis Sanchez <johanalexis54@gmail.com>
2026-01-09 19:38:54 -0500

    Complete Exersice 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string Cont;
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(0,101);
+            Console.WriteLine("Guess the magic number (0-100)! ");
+  
+            int guess = -1;
+            while (guess != number)
+            {
+                Console.WriteLine("What is your guess? ");
+                string response = Console.ReadLine();
+                guess = int.Parse(response);
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine("Do you want to play again? (yes/no): ");
+            Cont = Console.ReadLine().ToLower();
+
+        } while (Cont == "yes");
+        
     }
 }
