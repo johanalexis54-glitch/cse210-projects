0d2e69b32cb4992a36ad7e575c7adf0320fe68c4
J.Alexis Sanchez <johanalexis54@gmail.com>
2026-01-14 00:33:58 -0500

    The first Worg

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Job job1 = new Job();
+        job1._jobTitle ="Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle ="Mechanical";
+        job2._company ="Apple";
+        job2._startYear = 2020;
+        job2._endYear= 2025;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._personName ="Alexis Sanchez";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        Console.WriteLine("Primer trabajo: "+ myResume._jobs[0]._jobTitle);
+
+        myResume.Display();
+
     }
 }
+
---
+++ week02/Journal/Resume.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _personName;
+    public List<Job> _jobs = new List<Job>();
+
+   public void Display()
+    {
+        Console.WriteLine("Resume of: " + _personName);
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
+     
